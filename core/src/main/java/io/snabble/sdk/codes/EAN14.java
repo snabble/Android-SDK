dd950f2417893b247fada26cce784d51e0f7025b
Alexander Junggeburth <alexander.junggeburth@snabble.io>
2018-07-10 13:27:18 +0200

    EAN14 support

---
+++ core/src/main/java/io/snabble/sdk/codes/EAN14.java
@@ -0,0 +1,15 @@
+package io.snabble.sdk.codes;
+
+public class EAN14 extends ScannableCode {
+    public EAN14(String code) {
+        super(code);
+
+        if (code.length() == 16) {
+            this.code = code.substring(2, code.length());
+        }
+    }
+
+    public static boolean isEan14(String code) {
+        return code != null && (code.length() == 16 && code.startsWith("01") || code.length() == 14);
+    }
+}
--- core/src/main/java/io/snabble/sdk/codes/ScannableCode.java
+++ core/src/main/java/io/snabble/sdk/codes/ScannableCode.java
@@ -5,13 +5,13 @@ import java.io.Serializable;
 import io.snabble.sdk.SnabbleSdk;
 
 public class ScannableCode implements Serializable {
-    private String code;
+    protected String code;
 
-    public ScannableCode(String code){
+    public ScannableCode(String code) {
         this.code = code;
     }
 
-    public String getCode(){
+    public String getCode() {
         return code;
     }
 
@@ -35,7 +35,7 @@ public class ScannableCode implements Serializable {
         return false;
     }
 
-    public boolean hasEmbeddedData(){
+    public boolean hasEmbeddedData() {
         return false;
     }
 
@@ -43,9 +43,11 @@ public class ScannableCode implements Serializable {
         return true;
     }
 
-    public static ScannableCode parse(SnabbleSdk snabbleSdk, String code){
-        if(EAN13.isEan13(code)){
+    public static ScannableCode parse(SnabbleSdk snabbleSdk, String code) {
+        if (EAN13.isEan13(code)) {
             return new EAN13(code, snabbleSdk);
+        } else if (EAN14.isEan14(code)) {
+            return new EAN14(code);
         } else {
             return new ScannableCode(code);
         }
