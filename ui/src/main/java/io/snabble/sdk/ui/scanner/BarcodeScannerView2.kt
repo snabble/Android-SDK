022b3302165ee3a568e96ad20bc2066eafcee26e
Alexander Junggeburth <alexander.junggeburth@snabble.io>
2020-12-21 17:00:12 +0100

    [wip] show preview and scan barcodes using camerax

--- ui/src/main/java/io/snabble/sdk/ui/scanner/BarcodeScannerView2.kt
+++ ui/src/main/java/io/snabble/sdk/ui/scanner/BarcodeScannerView2.kt
@@ -1,31 +1,34 @@
 package io.snabble.sdk.ui.scanner
 
 import android.Manifest
+import android.R.attr
 import android.content.Context
 import android.content.pm.PackageManager
+import android.graphics.Rect
 import android.util.AttributeSet
-import android.util.Size
-import android.view.Gravity
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.FrameLayout
 import android.widget.TextView
-import androidx.camera.core.AspectRatio
-import androidx.camera.core.CameraSelector
-import androidx.camera.core.Preview
+import android.widget.Toast
+import androidx.camera.core.*
 import androidx.camera.lifecycle.ProcessCameraProvider
-import androidx.camera.view.CameraView
 import androidx.camera.view.PreviewView
 import androidx.core.app.ActivityCompat
-import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.core.content.ContextCompat
 import com.google.common.util.concurrent.ListenableFuture
 import io.snabble.sdk.BarcodeFormat
 import io.snabble.sdk.ui.R
 import io.snabble.sdk.ui.utils.UIUtils
-import java.lang.Exception
+import io.snabble.sdk.utils.Dispatch
+import kotlinx.android.synthetic.main.snabble_item_checkout_offline_qrcode.view.*
+import java.nio.ByteBuffer
 import java.util.*
-import java.util.concurrent.ExecutionException
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
+import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
 
 
 /**
@@ -38,13 +41,24 @@ import java.util.concurrent.ExecutionException
 class BarcodeScannerView2 @JvmOverloads constructor(
         context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : FrameLayout(context, attrs, defStyleAttr) {
+    companion object {
+        private const val RATIO_4_3_VALUE = 4.0 / 3.0
+        private const val RATIO_16_9_VALUE = 16.0 / 9.0
+    }
+
+    var restrictionOvershoot: Float? = 0.0f
+
+    private var cameraProvider: ProcessCameraProvider? = null
+    private var imageAnalyzer: ImageAnalysis? = null
+    private var preview: Preview? = null
+    private var cameraSelector: CameraSelector? = null
+    private var camera: androidx.camera.core.Camera? = null
+    private var cameraExecutor: ExecutorService? = null
     private val supportedBarcodeFormats: MutableList<BarcodeFormat> = ArrayList()
 
-    private var previewView: PreviewView
-
-//    private var splashView: FrameLayout
-//    private var cameraUnavailableView: TextView
-//    private var scanIndicatorView: ScanIndicatorView
+    private val previewView: PreviewView
+    private var cameraUnavailableView: TextView
+    private var scanIndicatorView: ScanIndicatorView
 
     private var indicatorEnabled = true
     private var barcodeDetector: BarcodeDetector
@@ -57,30 +71,23 @@ class BarcodeScannerView2 @JvmOverloads constructor(
                 ViewGroup.LayoutParams.MATCH_PARENT)
         addView(previewView)
 
-//        scanIndicatorView = ScanIndicatorView(context)
-//        scanIndicatorView.layoutParams = ViewGroup.LayoutParams(
-//                ViewGroup.LayoutParams.MATCH_PARENT,
-//                ViewGroup.LayoutParams.MATCH_PARENT)
-//        addView(scanIndicatorView)
-//
-//        splashView = FrameLayout(context)
-//        splashView.layoutParams = ViewGroup.LayoutParams(
-//                ViewGroup.LayoutParams.MATCH_PARENT,
-//                ViewGroup.LayoutParams.MATCH_PARENT)
-//        splashView.setBackgroundResource(UIUtils.getColorByAttribute(context, android.R.attr.background))
-//        addView(splashView)
-//
-//        cameraUnavailableView = TextView(context)
-//        cameraUnavailableView.layoutParams = ViewGroup.LayoutParams(
-//                ViewGroup.LayoutParams.MATCH_PARENT,
-//                ViewGroup.LayoutParams.MATCH_PARENT)
-//        cameraUnavailableView.setText(R.string.Snabble_Scanner_Camera_accessDenied)
-//        cameraUnavailableView.gravity = Gravity.CENTER
-//        cameraUnavailableView.setTextColor(UIUtils.getColorByAttribute(context, android.R.attr.textColorPrimary))
-//        cameraUnavailableView.setBackgroundResource(UIUtils.getColorByAttribute(context, android.R.attr.background))
-//        cameraUnavailableView.visibility = GONE
-//
-//        addView(cameraUnavailableView)
+        scanIndicatorView = ScanIndicatorView(context)
+        scanIndicatorView.layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT)
+        addView(scanIndicatorView)
+
+        cameraUnavailableView = TextView(context)
+        cameraUnavailableView.layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT)
+        cameraUnavailableView.setText(R.string.Snabble_Scanner_Camera_accessDenied)
+        cameraUnavailableView.gravity = Gravity.CENTER
+        cameraUnavailableView.setTextColor(UIUtils.getColorByAttribute(context, android.R.attr.textColorPrimary))
+        cameraUnavailableView.setBackgroundResource(UIUtils.getColorByAttribute(context, android.R.attr.background))
+        cameraUnavailableView.visibility = GONE
+
+        addView(cameraUnavailableView)
 
         barcodeDetector = BarcodeDetectorFactory.getDefaultBarcodeDetectorFactory().create()
     }
@@ -103,6 +110,8 @@ class BarcodeScannerView2 @JvmOverloads constructor(
             throw RuntimeException("Missing camera permission")
         }
 
+        cameraExecutor = Executors.newSingleThreadExecutor()
+
         previewView.post {
             setCameraProviderListener()
         }
@@ -119,37 +128,93 @@ class BarcodeScannerView2 @JvmOverloads constructor(
         }, ContextCompat.getMainExecutor(context))
     }
 
-    private fun bindPreview(cameraProvider: ProcessCameraProvider) {
-        val preview : Preview = Preview.Builder()
-                .setTargetAspectRatio(AspectRatio.RATIO_16_9)
-                .build()
+    private fun bindPreview(cp: ProcessCameraProvider) {
+        val rotation = previewView.display.rotation
 
-        val cameraSelector : CameraSelector = CameraSelector.Builder()
+        cameraProvider = cp
+
+        cameraSelector = CameraSelector.Builder()
                 .requireLensFacing(CameraSelector.LENS_FACING_BACK)
                 .build()
 
+        preview = Preview.Builder()
+                .setTargetAspectRatio(AspectRatio.RATIO_16_9)
+                .setTargetRotation(rotation)
+                .build()
 
-        var camera = cameraProvider.bindToLifecycle(UIUtils.getHostFragmentActivity(context), cameraSelector, preview)
-        preview.setSurfaceProvider(previewView.surfaceProvider)
+        imageAnalyzer = ImageAnalysis.Builder()
+                .setTargetAspectRatio(AspectRatio.RATIO_16_9)
+                .setTargetRotation(rotation)
+                .build()
+                .also {
+                    // TODO remove !!
+                    it.setAnalyzer(cameraExecutor!!, { image ->
+                        val y = image.planes[0].buffer // Y plane of a YUV buffer
+                        val rect = if (scanIndicatorView.visibility == View.VISIBLE) {
+                            val height = image.height / 3
+                            val top = (image.height / 2) - height
+                            val bottom = (image.height / 2) + height
+                            Rect(0, top, image.width, bottom)
+                        } else {
+                            Rect(0, 0, image.width, image.height)
+                        }
+
+                        val barcode = barcodeDetector.detect(y.toByteArray(),
+                                image.width, image.height, 8, rect, previewView.display.rotation)
+
+                        if (barcode != null) {
+                            Dispatch.mainThread {
+                                callback?.onBarcodeDetected(barcode)
+                            }
+                        }
+                        image.close()
+                    })
+                }
+
+        cameraProvider?.unbindAll()
+        resume()
+        preview?.setSurfaceProvider(previewView.surfaceProvider)
+    }
+
+    private fun ByteBuffer.toByteArray(): ByteArray {
+        rewind()
+        val data = ByteArray(remaining())
+        get(data)
+        return data
     }
 
     fun stop() {
-
+        pause()
+        cameraExecutor?.shutdown()
     }
 
     /**
      * Pauses the camera preview and barcode scanning, but keeps the camera intact.
      */
     fun pause() {
-
+        cameraProvider?.unbindAll()
     }
 
     /**
      * Resumes the camera preview and barcode scanning.
      */
     fun resume() {
+        try {
+            camera = cameraProvider?.bindToLifecycle(UIUtils.getHostFragmentActivity(context), cameraSelector!!, preview, imageAnalyzer)
 
+            val factory = previewView.meteringPointFactory
+            val dm = resources.displayMetrics
+            val point = factory.createPoint((dm.widthPixels / 2).toFloat(), (dm.heightPixels / 2).toFloat())
+            val action = FocusMeteringAction.Builder(point)
+                    .setAutoCancelDuration(2, TimeUnit.SECONDS)
+                    .build()
+            camera?.cameraControl?.startFocusAndMetering(action)
+        } catch (e: Exception) {
+            cameraUnavailableView.visibility = View.VISIBLE
+            scanIndicatorView.visibility = View.GONE
+        }
     }
+
     /**
      * @return Returns true if the torch is enabled, false otherwise.
      */
@@ -160,7 +225,9 @@ class BarcodeScannerView2 @JvmOverloads constructor(
      */
     var isTorchEnabled: Boolean
         get() = false
-        set(enabled) {}
+        set(enabled) {
+            camera?.cameraControl?.enableTorch(enabled)
+        }
 
     private fun setupBarcodeDetector() {
 
@@ -200,7 +267,7 @@ class BarcodeScannerView2 @JvmOverloads constructor(
      * @param scale the scale of the [ScanIndicatorView]. Defaults to 1.
      */
     fun setIndicatorScale(scale: Float) {
-       // scanIndicatorView!!.setScale(scale)
+       scanIndicatorView.setScale(scale)
     }
 
     /**
@@ -214,7 +281,7 @@ class BarcodeScannerView2 @JvmOverloads constructor(
     }
 
     fun setIndicatorStyle(style: ScanIndicatorView.Style?) {
-        //scanIndicatorView!!.setStyle(style)
+        scanIndicatorView.setStyle(style)
     }
 
     /**
@@ -223,36 +290,20 @@ class BarcodeScannerView2 @JvmOverloads constructor(
      * setIndicatorStyle(ScanIndicatorView.Style.NORMALIZED)
      */
     fun setIndicatorNormalizedSize(left: Float, top: Float, right: Float, bottom: Float) {
-        //scanIndicatorView!!.setNormalizedSize(left, top, right, bottom)
+        scanIndicatorView.setNormalizedSize(left, top, right, bottom)
     }
 
     private fun setScanIndicatorVisible(visible: Boolean) {
-//        if (indicatorEnabled && visible) {
-//            scanIndicatorView!!.visibility = VISIBLE
-//        } else {
-//            scanIndicatorView!!.visibility = GONE
-//        }
+        if (indicatorEnabled && visible) {
+            scanIndicatorView.visibility = VISIBLE
+        } else {
+            scanIndicatorView.visibility = GONE
+        }
     }
 
     fun setIndicatorOffset(offsetX: Int, offsetY: Int) {
-        //scanIndicatorView!!.setOffset(offsetX, offsetY)
+        scanIndicatorView.setOffset(offsetX, offsetY)
     }
-
-    /**
-     * Set to true, if you want to restrict barcode scanning to the center.
-     *
-     *
-     * Default: true
-     */
-    fun setRestrictScanningToIndicator(restrictScanningToIndicator: Boolean) {}
-
-    /**
-     * Sets the multiplicaton value that is used to allow scanning outside of the indicator if
-     * [.setRestrictScanningToIndicator] is set
-     *
-     * Default is 1.0
-     */
-    fun setRestrictionOvershoot(`val`: Float) {}
 
     /**
      * Sets a callback to get scan results.
