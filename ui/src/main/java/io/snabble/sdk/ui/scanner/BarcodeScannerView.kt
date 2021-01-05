708ac80135b0db35f5ef9b461c1c1072eb05e698
Alexander Junggeburth <alexander.junggeburth@snabble.io>
2021-01-05 11:59:43 +0100

    cleanup

--- ui/src/main/java/io/snabble/sdk/ui/scanner/BarcodeScannerView.kt
+++ ui/src/main/java/io/snabble/sdk/ui/scanner/BarcodeScannerView.kt
@@ -15,7 +15,6 @@ import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import com.google.common.util.concurrent.ListenableFuture
 import io.snabble.sdk.BarcodeFormat
 import io.snabble.sdk.ui.R
 import io.snabble.sdk.ui.utils.UIUtils
