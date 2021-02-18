50320161835d63fc6c6aaebbc4b455357076169f
Alexander Junggeburth <alexander.junggeburth@snabble.io>
2021-02-18 10:07:30 +0100

    add SnabbleFileProvider

---
+++ core/src/main/java/io/snabble/sdk/SnabbleFileProvider.java
@@ -0,0 +1,8 @@
+package io.snabble.sdk;
+
+/**
+ * Tagging subclass to avoid clashes with misbehaving libraries.
+ */
+public class SnabbleFileProvider extends androidx.core.content.FileProvider {
+
+}
