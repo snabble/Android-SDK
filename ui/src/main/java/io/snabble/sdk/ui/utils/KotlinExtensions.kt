a1a71222b5d21f83dd4defc2c0b5f58e720ab93e
René Kilczan <rene.kilczan@snabble.io>
2021-06-01 12:31:23 +0200

    Fix highlighting with different cases

--- ui/src/main/java/io/snabble/sdk/ui/utils/KotlinExtensions.kt
+++ ui/src/main/java/io/snabble/sdk/ui/utils/KotlinExtensions.kt
@@ -23,22 +23,28 @@ import kotlin.math.max
 fun CharSequence?.isNotNullOrBlank() = !isNullOrBlank()
 
 fun String.highlight(query: String): SpannableString {
-    val normalizedText = StringNormalizer.normalize(this)
+    val normalizedText = StringNormalizer.normalize(lowercase())
     val sb = SpannableString(this)
-    query.split(" ")
-            .filter { it.isNotEmpty() }
-            .forEach { q ->
-                var lastIndex = 0
-                while (true) {
-                    lastIndex = normalizedText.indexOf(q, lastIndex)
-                    if (lastIndex == -1) {
-                        break
-                    }
-                    val styleSpan = StyleSpan(Typeface.BOLD)
-                    sb.setSpan(styleSpan, lastIndex, lastIndex + q.length, Spannable.SPAN_INCLUSIVE_EXCLUSIVE)
-                    lastIndex += q.length
+    query.lowercase()
+        .split(" ")
+        .filter { it.isNotEmpty() }
+        .forEach { q ->
+            var lastIndex = 0
+            while (true) {
+                lastIndex = normalizedText.indexOf(q, lastIndex)
+                if (lastIndex == -1) {
+                    break
                 }
+                val styleSpan = StyleSpan(Typeface.BOLD)
+                sb.setSpan(
+                    styleSpan,
+                    lastIndex,
+                    lastIndex + q.length,
+                    Spannable.SPAN_INCLUSIVE_EXCLUSIVE
+                )
+                lastIndex += q.length
             }
+        }
     return sb
 }
 
