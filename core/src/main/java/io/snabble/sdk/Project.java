f81d8c8770c264f7939114dcca9e7f4a07b1aeb4
René Kilczan <rene.kilczan@snabble.io>
2021-06-28 11:55:23 +0200

    Expose ShoppingListDbUrl

--- core/src/main/java/io/snabble/sdk/Project.java
+++ core/src/main/java/io/snabble/sdk/Project.java
@@ -385,6 +385,10 @@ public class Project {
         return urls.get("datatransTokenization");
     }
 
+    public String getShoppingListDbUrl() {
+        return urls.get("shoppingListDB");
+    }
+
     public BarcodeFormat[] getSupportedBarcodeFormats() {
         return supportedBarcodeFormats;
     }
