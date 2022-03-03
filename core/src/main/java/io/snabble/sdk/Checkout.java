a8aeb1ef3a93b81f278432ae8a3f7188b35f10a7
Alexander Junggeburth <alexander.junggeburth@snabble.io>
2022-03-03 13:05:07 +0100

    fix crashes when offline

--- core/src/main/java/io/snabble/sdk/Checkout.java
+++ core/src/main/java/io/snabble/sdk/Checkout.java
@@ -713,7 +713,9 @@ public class Checkout {
                 shoppingCart.backup();
             }
 
-            redeemedCoupons = signedCheckoutInfo.getRedeemedCoupons(project.getCoupons().get());
+            if (signedCheckoutInfo != null) {
+                redeemedCoupons = signedCheckoutInfo.getRedeemedCoupons(project.getCoupons().get());
+            }
             shoppingCart.invalidate();
             clearCodes();
             notifyStateChanged(Checkout.State.PAYMENT_APPROVED);
