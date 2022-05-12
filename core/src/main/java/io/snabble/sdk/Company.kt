d105368c28e585a92ade3e80e7723d163c2e4595
Alexander Junggeburth <alexander.junggeburth@snabble.io>
2022-05-12 08:25:45 +0200

    docs

--- core/src/main/java/io/snabble/sdk/Company.kt
+++ core/src/main/java/io/snabble/sdk/Company.kt
@@ -1,9 +1,27 @@
 package io.snabble.sdk
 
+/**
+ * Class describing a company
+ */
 data class Company(
+    /**
+     * Get the name of the city
+     */
     val city: String,
+    /**
+     * Get the name of the country
+     */
     val country: String,
+    /**
+     * Get the companies name
+     */
     val name: String,
+    /**
+     * Get the name of the street, including the house number
+     */
     val street: String,
+    /**
+     * Get the zip code
+     */
     val zip: String
 )
