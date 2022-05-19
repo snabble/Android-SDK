6b40b8dc75b5c3b392bc36af94507785ab26c838
Alexander Junggeburth <alexander.junggeburth@snabble.io>
2022-05-19 13:32:44 +0200

    Document terms of service

--- core/src/main/java/io/snabble/sdk/TermsOfService.kt
+++ core/src/main/java/io/snabble/sdk/TermsOfService.kt
@@ -1,55 +1,78 @@
-package io.snabble.sdk;
+package io.snabble.sdk
 
-import java.util.List;
-import java.util.Locale;
+import java.util.*
 
-public class TermsOfService {
-    public String updatedAt;
-    public String version;
-    public List<Variant> variants;
-
-    public String getHtmlLinkForSystemLanguage() {
-        Locale defaultLocale = Locale.getDefault();
-        for (Variant variant : variants) {
-            if (new Locale(variant.language).getLanguage().equals(defaultLocale.getLanguage())) {
-                return Snabble.getInstance().absoluteUrl(variant.getUrl());
-            }
-        }
-
-        for (Variant variant : variants) {
-            if (variant.isDefault) {
-                return Snabble.getInstance().absoluteUrl(variant.getUrl());
-            }
-        }
-
-        if (variants.size() > 0) {
-            return Snabble.getInstance().absoluteUrl(variants.get(0).getUrl());
-        }
-
-        return null;
-    }
-
-    public static class Variant {
-        public boolean isDefault;
-        public String language;
-        public Links links;
-
-        public String getUrl() {
-            if (links != null && links.content != null) {
-                return links.content.href;
+/**
+ * Class for describing our terms of service.
+ */
+data class TermsOfService(
+    val updatedAt: String,
+    val version: String,
+    val variants: List<Variant> = emptyList()
+) {
+    /**
+     * Gets a link to a downloadable html in the current system language or a default if no
+     * translation is available.
+     */
+    val htmlLinkForSystemLanguage: String?
+        get() {
+            val defaultLocale = Locale.getDefault()
+            variants.forEach {
+                if (Locale(it.language).language == defaultLocale.language) {
+                    return Snabble.absoluteUrl(it.url)
+                }
             }
 
-            return  null;
+            variants.forEach {
+                if (it.isDefault) {
+                    return Snabble.absoluteUrl(it.url)
+                }
+            }
+
+            return if (variants.isNotEmpty()) {
+                variants[0].let { Snabble.absoluteUrl(it.url) }
+            } else null
         }
+
+    /**
+     * A variant of the terms of service document
+     */
+    data class Variant(
+        /**
+         * True if this is the default terms of service to display if
+         * a language specific one is not available
+         */
+        val isDefault: Boolean = false,
+        /**
+         * The iso2 language of the document
+         */
+        val language: String,
+        /**
+         * Link to the html document
+         */
+        val links: Links
+    ) {
+        /**
+         * The relative url of the html document
+         */
+        val url: String
+            get() = links.content.href
     }
 
-    public static class Links {
-        private Content content;
-    }
+    /**
+     * Class for link encapsulation
+     */
+    data class Links(
+        val content: Content
+    )
 
-    public static class Content {
-        private String href;
-    }
+    /**
+     * Class for link encapsulation
+     */
+    data class Content(
+        /**
+         * The relative url of the html document
+         */
+        val href: String
+    )
 }
-
-
