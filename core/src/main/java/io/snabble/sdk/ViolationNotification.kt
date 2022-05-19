0663cf78d85887e5588412f582b97ba70e4702a0
Alexander Junggeburth <alexander.junggeburth@snabble.io>
2022-05-19 12:12:32 +0200

    add docs to violation notification

--- core/src/main/java/io/snabble/sdk/ViolationNotification.kt
+++ core/src/main/java/io/snabble/sdk/ViolationNotification.kt
@@ -4,8 +4,14 @@ package io.snabble.sdk
  * A notification that a violation accrued.
  */
 data class ViolationNotification(
+    /** The name of the affected violation item, e.g. a coupon **/
     val name: String?,
+    /** The local generated uuid of the affected item **/
     val refersTo: String?,
+    /** The type of the violation **/
     val type: String? = null,
+    /** Non-localized message of the backend which sould be displayed
+     * if no localized message is available
+     */
     val fallbackMessage: String? = null,
 )
