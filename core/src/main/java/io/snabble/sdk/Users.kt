069cf9025fd75bc8dfef9d642af9b3f6756a2df4
Alexander Junggeburth <alexander.junggeburth@snabble.io>
2022-05-19 12:51:04 +0200

    convert users to kotlin, add restrictTo annotations, internal modifiers and docs

--- core/src/main/java/io/snabble/sdk/Users.kt
+++ core/src/main/java/io/snabble/sdk/Users.kt
@@ -1,222 +1,225 @@
-package io.snabble.sdk;
+package io.snabble.sdk
 
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
-import java.util.TimeZone;
+import androidx.annotation.RestrictTo
+import io.snabble.sdk.utils.SimpleJsonCallback
+import io.snabble.sdk.utils.GsonHolder
+import io.snabble.sdk.UserPreferences.ConsentStatus
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.Request.*
+import okhttp3.RequestBody.Companion.toRequestBody
+import java.io.IOException
+import java.lang.Exception
+import java.text.SimpleDateFormat
+import java.util.*
 
-import io.snabble.sdk.auth.AppUser;
-import io.snabble.sdk.utils.GsonHolder;
-import io.snabble.sdk.utils.SimpleJsonCallback;
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.MediaType;
-import okhttp3.OkHttpClient;
-import okhttp3.RequestBody;
-
-public class Users {
-    public interface UpdateUserCallback {
-        void success();
-        void failure();
+/**
+ * Class managing per-user settings of the backend
+ */
+class Users
+@RestrictTo(RestrictTo.Scope.LIBRARY)
+internal constructor(userPreferences: UserPreferences) {
+    interface UpdateUserCallback {
+        fun success()
+        fun failure()
     }
 
-    public interface GetUserCallback {
-        void success(Response response);
-        void failure();
+    interface GetUserCallback {
+        fun success(response: Response)
+        fun failure()
     }
 
-    private static class Request {
-        public String id;
-        public String dayOfBirth;
+    private class Request {
+        var id: String? = null
+        var dayOfBirth: String? = null
     }
 
-    public static class Response {
-        public String id;
-        public String dayOfBirth;
-        public String bornBeforeOrOn;
+    class Response {
+        var id: String? = null
+        var dayOfBirth: String? = null
+        var bornBeforeOrOn: String? = null
     }
 
-    private static class UpdateConsentRequest {
-        public String version;
+    private class UpdateConsentRequest {
+        var version: String? = null
     }
 
-    private Call updateBirthdayCall;
-    private Call postConsentCall;
-    private final SimpleDateFormat simpleDateFormat;
-    private final UserPreferences userPreferences;
+    private var updateBirthdayCall: Call? = null
+    private var postConsentCall: Call? = null
+    private val simpleDateFormat: SimpleDateFormat = SimpleDateFormat("yyyy/MM/dd", Locale.US)
+    private val userPreferences: UserPreferences
 
-    public Users(UserPreferences userPreferences) {
-        simpleDateFormat = new SimpleDateFormat("yyyy/MM/dd", Locale.US);
-        simpleDateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
-
-        this.userPreferences = userPreferences;
+    init {
+        simpleDateFormat.timeZone = TimeZone.getTimeZone("UTC")
+        this.userPreferences = userPreferences
     }
 
-    public void get(final GetUserCallback callback) {
-        final Snabble snabble = Snabble.getInstance();
-        String url = snabble.getUsersUrl();
-
-        AppUser appUser = snabble.getUserPreferences().getAppUser();
-
+    /**
+     * Gets the current information about the user from the backend.
+     */
+    operator fun get(callback: GetUserCallback) {
+        val url = Snabble.usersUrl
+        val appUser = Snabble.userPreferences.appUser
         if (appUser != null && url != null) {
-            okhttp3.Request request = new okhttp3.Request.Builder()
-                    .get()
-                    .url(url.replace("{appUserID}", appUser.id))
-                    .build();
-
-            OkHttpClient okHttpClient = snabble.getProjects().get(0).getOkHttpClient();
-            okHttpClient.newCall(request).enqueue(new SimpleJsonCallback<Response>(Response.class) {
-                @Override
-                public void success(Response response) {
-                    callback.success(response);
-                }
-
-                @Override
-                public void error(Throwable t) {
-                    callback.failure();
-                }
-            });
-        } else {
-            callback.failure();
-        }
-    }
-
-    public void setBirthday(Date birthday, final UpdateUserCallback updateUserCallback) {
-        final Snabble snabble = Snabble.getInstance();
-        String url = snabble.getUsersUrl();
-
-        AppUser appUser = snabble.getUserPreferences().getAppUser();
-
-        if (appUser != null && url != null) {
-            Request updateBirthdayRequest = new Request();
-            updateBirthdayRequest.id = appUser.id;
-            updateBirthdayRequest.dayOfBirth = simpleDateFormat.format(birthday);
-            RequestBody requestBody = RequestBody.create(MediaType.parse("application/json"),
-                    GsonHolder.get().toJson(updateBirthdayRequest));
-
-            okhttp3.Request request = new okhttp3.Request.Builder()
-                    .patch(requestBody)
-                    .url(url.replace("{appUserID}", appUser.id))
-                    .build();
-
-            OkHttpClient okHttpClient = snabble.getProjects().get(0).getOkHttpClient();
-            updateBirthdayCall = okHttpClient.newCall(request);
-            updateBirthdayCall.enqueue(new Callback() {
-                @Override
-                public void onFailure(Call call, IOException e) {
-                    updateBirthdayCall = null;
-                    updateUserCallback.failure();
-                }
-
-                @Override
-                public void onResponse(Call call, okhttp3.Response response) {
-                    if (response.isSuccessful()) {
-                        updateBirthdayCall = null;
-                        updateUserCallback.success();
-                    } else {
-                        updateBirthdayCall = null;
-                        updateUserCallback.failure();
-                    }
-                }
-            });
-        } else {
-            updateUserCallback.failure();
-        }
-    }
-
-    public void postPendingConsents() {
-        UserPreferences.ConsentStatus consentStatus = userPreferences.getConsentStatus();
-        if (consentStatus == UserPreferences.ConsentStatus.TRANSMIT_FAILED) {
-            postConsentVersion();
-        } else if (consentStatus == UserPreferences.ConsentStatus.TRANSMITTING) {
-            if (postConsentCall == null) {
-                postConsentVersion();
-            }
-        }
-    }
-
-    public void setConsent(String version) {
-        userPreferences.setConsentVersion(version);
-        postConsentVersion();
-    }
-
-    private void postConsentVersion() {
-        final Snabble snabble = Snabble.getInstance();
-        String url = snabble.getConsentUrl();
-        AppUser appUser = userPreferences.getAppUser();
-
-        if (appUser == null || url == null) {
-            return;
-        }
-
-        String version = userPreferences.getConsentVersion();
-        UpdateConsentRequest updateBirthdayRequest = new UpdateConsentRequest();
-        updateBirthdayRequest.version = version;
-        RequestBody requestBody = RequestBody.create(MediaType.parse("application/json"),
-                GsonHolder.get().toJson(updateBirthdayRequest));
-
-        okhttp3.Request request = new okhttp3.Request.Builder()
-                .post(requestBody)
+            val request = Builder()
+                .get()
                 .url(url.replace("{appUserID}", appUser.id))
-                .build();
+                .build()
 
-        userPreferences.setConsentStatus(UserPreferences.ConsentStatus.TRANSMITTING);
+            val okHttpClient = Snabble.projects[0].okHttpClient
+            okHttpClient.newCall(request).enqueue(object : SimpleJsonCallback<Response>(
+                Response::class.java
+            ) {
+                override fun success(response: Response) {
+                    callback.success(response)
+                }
 
-        OkHttpClient okHttpClient = snabble.getProjects().get(0).getOkHttpClient();
-        postConsentCall = okHttpClient.newCall(request);
-        postConsentCall.enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-                postConsentCall = null;
-                userPreferences.setConsentStatus(UserPreferences.ConsentStatus.TRANSMIT_FAILED);
+                override fun error(t: Throwable) {
+                    callback.failure()
+                }
+            })
+        } else {
+            callback.failure()
+        }
+    }
+
+    /**
+     * Sets the birthday of the user.
+     */
+    fun setBirthday(birthday: Date, updateUserCallback: UpdateUserCallback) {
+        val url = Snabble.usersUrl
+        val appUser = Snabble.userPreferences.appUser
+        if (appUser != null && url != null) {
+            val updateBirthdayRequest = Request()
+            updateBirthdayRequest.id = appUser.id
+            updateBirthdayRequest.dayOfBirth = simpleDateFormat.format(birthday)
+
+            val requestBody = GsonHolder.get().toJson(updateBirthdayRequest)
+                .toRequestBody("application/json".toMediaType())
+
+            val request: okhttp3.Request = Builder()
+                .patch(requestBody)
+                .url(url.replace("{appUserID}", appUser.id))
+                .build()
+            val okHttpClient = Snabble.projects[0].okHttpClient
+
+            updateBirthdayCall = okHttpClient.newCall(request)
+            updateBirthdayCall?.enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    updateBirthdayCall = null
+                    updateUserCallback.failure()
+                }
+
+                override fun onResponse(call: Call, response: okhttp3.Response) {
+                    if (response.isSuccessful) {
+                        updateBirthdayCall = null
+                        updateUserCallback.success()
+                    } else {
+                        updateBirthdayCall = null
+                        updateUserCallback.failure()
+                    }
+                }
+            })
+        } else {
+            updateUserCallback.failure()
+        }
+    }
+
+    @RestrictTo(RestrictTo.Scope.LIBRARY)
+    internal fun postPendingConsents() {
+        val consentStatus = userPreferences.consentStatus
+        if (consentStatus === ConsentStatus.TRANSMIT_FAILED) {
+            postConsentVersion()
+        } else if (consentStatus === ConsentStatus.TRANSMITTING) {
+            if (postConsentCall == null) {
+                postConsentVersion()
+            }
+        }
+    }
+
+    /**
+     * Updates the given consent of a user
+     *
+     * @param version The last version the user accepted
+     */
+    fun setConsent(version: String?) {
+        userPreferences.consentVersion = version
+        postConsentVersion()
+    }
+
+    private fun postConsentVersion() {
+        val url = Snabble.consentUrl
+        val appUser = userPreferences.appUser
+        if (appUser == null || url == null) {
+            return
+        }
+        val version = userPreferences.consentVersion
+
+        val updateBirthdayRequest = UpdateConsentRequest()
+        updateBirthdayRequest.version = version
+        val requestBody = GsonHolder.get().toJson(updateBirthdayRequest)
+            .toRequestBody("application/json".toMediaType())
+
+        val request: okhttp3.Request = Builder()
+            .post(requestBody)
+            .url(url.replace("{appUserID}", appUser.id))
+            .build()
+
+        userPreferences.consentStatus = ConsentStatus.TRANSMITTING
+
+        val okHttpClient = Snabble.projects[0].okHttpClient
+        postConsentCall = okHttpClient.newCall(request)
+        postConsentCall?.enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                postConsentCall = null
+                userPreferences.consentStatus = ConsentStatus.TRANSMIT_FAILED
             }
 
-            @Override
-            public void onResponse(Call call, okhttp3.Response response) {
-                if (response.isSuccessful()) {
-                    postConsentCall = null;
-                    userPreferences.setConsentStatus(UserPreferences.ConsentStatus.ACCEPTED);
+            override fun onResponse(call: Call, response: okhttp3.Response) {
+                if (response.isSuccessful) {
+                    postConsentCall = null
+                    userPreferences.consentStatus = ConsentStatus.ACCEPTED
                 } else {
-                    postConsentCall = null;
-                    if (response.code() == 400) {
-                        userPreferences.setConsentStatus(UserPreferences.ConsentStatus.ACCEPTED);
+                    postConsentCall = null
+                    if (response.code == 400) {
+                        userPreferences.consentStatus = ConsentStatus.ACCEPTED
                     } else {
-                        userPreferences.setConsentStatus(UserPreferences.ConsentStatus.TRANSMIT_FAILED);
+                        userPreferences.consentStatus = ConsentStatus.TRANSMIT_FAILED
                     }
                 }
             }
-        });
+        })
     }
 
-    public void update() {
-        get(new GetUserCallback() {
-            @Override
-            public void success(Response response) {
-                Date birthday = null;
+    @RestrictTo(RestrictTo.Scope.LIBRARY)
+    fun update() {
+        get(object : GetUserCallback {
+            override fun success(response: Response) {
+                var birthday: Date? = null
+
+                val dayOfBirth = response.dayOfBirth
+                val bornBeforeOrOn = response.bornBeforeOrOn
 
                 try {
-                    if (response.dayOfBirth != null) {
-                        birthday = simpleDateFormat.parse(response.dayOfBirth);
-                    } else if (response.bornBeforeOrOn != null) {
-                        birthday = simpleDateFormat.parse(response.bornBeforeOrOn);
+                    if (dayOfBirth != null) {
+                        birthday = simpleDateFormat.parse(dayOfBirth)
+                    } else if (bornBeforeOrOn != null) {
+                        birthday = simpleDateFormat.parse(bornBeforeOrOn)
                     }
-                } catch (Exception ignored) { }
+                } catch (ignored: Exception) { }
 
-                Snabble.getInstance().getUserPreferences().setBirthday(birthday);
+                Snabble.userPreferences.birthday = birthday
             }
 
-            @Override
-            public void failure() {
-
-            }
-        });
+            override fun failure() {}
+        })
     }
 
-    public void cancelUpdatingUser() {
+    @RestrictTo(RestrictTo.Scope.LIBRARY)
+    fun cancelUpdatingUser() {
         if (updateBirthdayCall != null) {
-            updateBirthdayCall.cancel();
+            updateBirthdayCall?.cancel()
         }
     }
-
 }
