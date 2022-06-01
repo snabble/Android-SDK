5c3723d7796028359f16151d2c335d3d3ee0bf35
Alexander Junggeburth <alexander.junggeburth@snabble.io>
2022-06-01 17:15:35 +0200

    separate data and bookkeeping of shopping cart

---
+++ core/src/main/java/io/snabble/sdk/ShoppingCartData.kt
@@ -0,0 +1,32 @@
+package io.snabble.sdk
+
+import io.snabble.sdk.utils.GsonHolder
+import java.util.*
+
+data class ShoppingCartData @JvmOverloads constructor(
+    @JvmField var id: String = UUID.randomUUID().toString(),
+    @JvmField var uuid: String? = UUID.randomUUID().toString(),
+    @JvmField var lastModificationTime: Long = 0,
+    @JvmSuppressWildcards @JvmField var items: List<ShoppingCart.Item> = emptyList(),
+    @JvmField var violationNotifications: List<ViolationNotification> = emptyList(),
+    @JvmField var modCount: Int = 0,
+    @JvmField var addCount: Int = 0,
+    @JvmField var onlineTotalPrice: Int? = null,
+    @JvmSuppressWildcards @JvmField var invalidProducts: List<Product>? = null,
+    @JvmField var taxation: ShoppingCart.Taxation = ShoppingCart.Taxation.UNDECIDED,
+    @JvmField var hasRaisedMaxCheckoutLimit: Boolean = false,
+    @JvmField var hasRaisedMaxOnlinePaymentLimit: Boolean = false,
+    @JvmField var invalidDepositReturnVoucher: Boolean = false,
+    @JvmField var backupTimestamp: Long = 0,
+) {
+    fun applyShoppingCart(shoppingCart: ShoppingCart) {
+        items.forEach {
+            it.cart = shoppingCart
+        }
+    }
+
+    fun deepCopy() : ShoppingCartData {
+        val json = GsonHolder.get().toJson(this)
+        return GsonHolder.get().fromJson<ShoppingCartData>(json, ShoppingCartData::class.java)
+    }
+}
