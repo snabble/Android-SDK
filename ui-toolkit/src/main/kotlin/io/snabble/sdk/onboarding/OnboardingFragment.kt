e96c18af5033985e0f7abece7d54863ef69629b1
Christian Maier <christian.maier@snabble.io>
2022-09-07 12:19:01 +0200

    Use StringRes as bundle key and small restructurings

--- ui-toolkit/src/main/kotlin/io/snabble/sdk/onboarding/OnboardingFragment.kt
+++ ui-toolkit/src/main/kotlin/io/snabble/sdk/onboarding/OnboardingFragment.kt
@@ -1,45 +1,30 @@
 package io.snabble.sdk.onboarding
 
-import android.annotation.SuppressLint
-import android.content.Context
-import android.net.Uri
 import android.os.Bundle
-import android.text.SpannedString
-import android.text.style.URLSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.accessibility.AccessibilityEvent
 import android.view.accessibility.AccessibilityNodeInfo
 import android.widget.Button
-import android.widget.FrameLayout
 import android.widget.ImageView
-import android.widget.TextView
 import androidx.activity.OnBackPressedCallback
-import androidx.core.os.bundleOf
-import androidx.core.text.getSpans
 import androidx.core.view.AccessibilityDelegateCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.accessibility.AccessibilityNodeInfoCompat
 import androidx.core.view.get
-import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
-import androidx.navigation.NavDeepLinkRequest
-import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
-import io.snabble.accessibility.accessibility
 import io.snabble.accessibility.isTalkBackActive
 import io.snabble.sdk.SnabbleUiToolkit
 import io.snabble.sdk.onboarding.entities.OnboardingItem
 import io.snabble.sdk.onboarding.entities.OnboardingModel
 import io.snabble.sdk.ui.toolkit.R
 import io.snabble.sdk.ui.utils.resolveImageOrHide
-import io.snabble.sdk.utils.LinkClickListener
 import io.snabble.sdk.utils.ZoomOutPageTransformer
-import io.snabble.sdk.utils.resolveTextOrHide
 
 open class OnboardingFragment : Fragment() {
     private lateinit var viewPager: ViewPager2
@@ -47,28 +32,32 @@ open class OnboardingFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View {
-        val v: View = inflater.inflate(R.layout.snabble_fragment_onboarding, container, false)
-        val model = arguments?.getParcelable<OnboardingModel>("model") ?: throw IllegalArgumentException()
-        val headerImage = v.findViewById<ImageView>(R.id.image_header)
+        val view: View = inflater
+            .inflate(R.layout.snabble_fragment_onboarding, container, false)
+        val model: OnboardingModel = arguments
+            ?.getParcelable(requireContext().getString(R.string.bundle_key_model))
+            ?: throw IllegalArgumentException("Missing necessary bundle args")
+        val headerImage = view.findViewById<ImageView>(R.id.image_header)
         headerImage.resolveImageOrHide(model.configuration?.imageSource)
 
-        viewPager = v.findViewById(R.id.view_pager)
-        viewPager.adapter = StepAdapter(model)
-        viewPager.offscreenPageLimit = 1
+        viewPager = view.findViewById<ViewPager2?>(R.id.view_pager).apply {
+            adapter = StepAdapter(model)
+            offscreenPageLimit = 1
+        }
         (viewPager.getChildAt(0) as? RecyclerView)?.setItemViewCacheSize(0)
 
-        val circleIndicator = v.findViewById<TabLayout>(R.id.circle_indicator)
+        val circleIndicator = view.findViewById<TabLayout>(R.id.circle_indicator)
         TabLayoutMediator(circleIndicator, viewPager) { _, _ -> }.attach()
 
-        val button = v.findViewById<Button>(R.id.button)
-
+        val button = view.findViewById<Button>(R.id.button)
         button.setOnClickListener {
             if (viewPager.currentItem < model.items.lastIndex) {
                 viewPager.currentItem += 1
             } else {
-                SnabbleUiToolkit.executeAction(requireContext(), SnabbleUiToolkit.Event.SHOW_ONBOARDING_DONE)
+                SnabbleUiToolkit
+                    .executeAction(requireContext(), SnabbleUiToolkit.Event.SHOW_ONBOARDING_DONE)
             }
         }
 
@@ -99,22 +88,29 @@ open class OnboardingFragment : Fragment() {
                     // to get the internal RecyclerView of the ViewPager2. There we take the
                     // ViewHolder to get the first child which should be OnboardingStepView.
                     // When a view could not be found fallback to the logical parent
-                    val viewPagerContent = viewPager.findViewHolderForAdapterPosition(viewPager.currentItem)?.itemView
+                    val viewPagerContent: View? = viewPager
+                        .findViewHolderForAdapterPosition(viewPager.currentItem)
+                        ?.itemView
                     val actualContent = (viewPagerContent as? ViewGroup)?.get(0)
-                    val view = actualContent ?: viewPagerContent ?: viewPager
+                    val contentView = actualContent ?: viewPagerContent ?: viewPager
 
                     // Focus the new OnboardingStepView to read the content
-                    view.performAccessibilityAction(AccessibilityNodeInfo.ACTION_ACCESSIBILITY_FOCUS, null)
-                    view.sendAccessibilityEvent(AccessibilityEvent.TYPE_VIEW_SELECTED)
+                    contentView.performAccessibilityAction(
+                        AccessibilityNodeInfo.ACTION_ACCESSIBILITY_FOCUS,
+                        null
+                    )
+                    contentView.sendAccessibilityEvent(AccessibilityEvent.TYPE_VIEW_SELECTED)
                 }
             }
 
             override fun onPageSelected(position: Int) {
                 val item = model.items[position]
                 button.text = item.customButtonTitle ?: getString(
-                    if (position == model.items.lastIndex)
+                    if (position == model.items.lastIndex) {
                         R.string.Snabble_Onboarding_done
-                    else R.string.Snabble_Onboarding_next
+                    } else {
+                        R.string.Snabble_Onboarding_next
+                    }
                 )
                 firstRun = false
             }
@@ -122,41 +118,45 @@ open class OnboardingFragment : Fragment() {
 
         // Change the accessibility order of items by code: Logo -> ViewPager -> Button -> Indicator
         ViewCompat.setAccessibilityDelegate(viewPager, object : AccessibilityDelegateCompat() {
-            override fun onInitializeAccessibilityNodeInfo(v: View, info: AccessibilityNodeInfoCompat) {
-                super.onInitializeAccessibilityNodeInfo(v, info)
+            override fun onInitializeAccessibilityNodeInfo(
+                view: View,
+                info: AccessibilityNodeInfoCompat,
+            ) {
+                super.onInitializeAccessibilityNodeInfo(view, info)
                 info.setTraversalBefore(button)
             }
         })
-        ViewCompat.setAccessibilityDelegate(circleIndicator, object : AccessibilityDelegateCompat() {
-            override fun onInitializeAccessibilityNodeInfo(v: View, info: AccessibilityNodeInfoCompat) {
-                super.onInitializeAccessibilityNodeInfo(v, info)
-                info.setTraversalAfter(button)
-            }
-        })
+        ViewCompat.setAccessibilityDelegate(circleIndicator,
+            object : AccessibilityDelegateCompat() {
+                override fun onInitializeAccessibilityNodeInfo(
+                    view: View,
+                    info: AccessibilityNodeInfoCompat,
+                ) {
+                    super.onInitializeAccessibilityNodeInfo(view, info)
+                    info.setTraversalAfter(button)
+                }
+            })
 
         viewPager.setPageTransformer(ZoomOutPageTransformer())
 
-        return v
+        return view
+    }
+}
+
+private class StepAdapter(
+    val onboardingModel: OnboardingModel,
+) : RecyclerView.Adapter<StepAdapter.StepViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StepViewHolder =
+        StepViewHolder(OnboardingStepView(parent.context))
+
+    override fun onBindViewHolder(holder: StepViewHolder, position: Int) {
+        holder.bind(onboardingModel.items[position])
     }
 
-    private class StepViewHolder(itemView: OnboardingStepView) : RecyclerView.ViewHolder(itemView) {
-        fun bind(data: OnboardingItem) {
-            (itemView as OnboardingStepView).bind(data)
-        }
-    }
+    override fun getItemCount(): Int = onboardingModel.items.size
 
-    private class StepAdapter(
-        val onboardingModel: OnboardingModel
-    ) : RecyclerView.Adapter<StepViewHolder>() {
-
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StepViewHolder =
-            StepViewHolder(OnboardingStepView(parent.context))
-
-        @SuppressLint("ResourceType")
-        override fun onBindViewHolder(holder: StepViewHolder, position: Int) {
-             holder.bind(onboardingModel.items[position])
-        }
-
-        override fun getItemCount() = onboardingModel.items.size
+    class StepViewHolder(itemView: OnboardingStepView) : RecyclerView.ViewHolder(itemView) {
+        fun bind(data: OnboardingItem) = (itemView as OnboardingStepView).bind(data)
     }
 }
