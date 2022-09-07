648ca4da5567453dcec4109c071e9263a52af0da
Christian Maier <christian.maier@snabble.io>
2022-09-07 12:42:05 +0200

    Refactor to smaller methods

--- ui-toolkit/src/main/kotlin/io/snabble/sdk/onboarding/OnboardingStepView.kt
+++ ui-toolkit/src/main/kotlin/io/snabble/sdk/onboarding/OnboardingStepView.kt
@@ -6,7 +6,10 @@ import android.text.method.LinkMovementMethod
 import android.text.style.URLSpan
 import android.util.AttributeSet
 import android.view.ViewGroup
-import android.widget.*
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.RelativeLayout
+import android.widget.TextView
 import androidx.core.os.bundleOf
 import androidx.core.text.getSpans
 import androidx.core.view.isVisible
@@ -21,7 +24,8 @@ import io.snabble.sdk.utils.resolveTextOrHide
 import io.snabble.sdk.utils.setTextOrHide
 
 class OnboardingStepView @JvmOverloads constructor(
-    context: Context, attrs: AttributeSet? = null
+    context: Context,
+    attrs: AttributeSet? = null,
 ) : RelativeLayout(context, attrs) {
     private val logo: ImageView
     private val text: TextView
@@ -41,28 +45,29 @@ class OnboardingStepView @JvmOverloads constructor(
         termsButton = findViewById(R.id.terms_button)
 
         attrs?.let {
-            context.theme.obtainStyledAttributes(attrs, R.styleable.OnboardingStepView, 0, 0).apply {
-                try {
-                    val titleText = getText(R.styleable.OnboardingStepView_title)
-                    val contentText = getText(R.styleable.OnboardingStepView_text)
-                    val footerText = getText(R.styleable.OnboardingStepView_footerText)
-                    val image = getResourceId(R.styleable.OnboardingStepView_image, -1)
+            context.theme.obtainStyledAttributes(attrs, R.styleable.OnboardingStepView, 0, 0)
+                .apply {
+                    try {
+                        val titleText = getText(R.styleable.OnboardingStepView_title)
+                        val contentText = getText(R.styleable.OnboardingStepView_text)
+                        val footerText = getText(R.styleable.OnboardingStepView_footerText)
+                        val image = getResourceId(R.styleable.OnboardingStepView_image, -1)
 
-                    title.setTextOrHide(titleText)
-                    text.setTextOrHide(contentText)
-                    footer.setTextOrHide(footerText)
-                    footer.movementMethod = LinkMovementMethod.getInstance()
+                        title.setTextOrHide(titleText)
+                        text.setTextOrHide(contentText)
+                        footer.setTextOrHide(footerText)
+                        footer.movementMethod = LinkMovementMethod.getInstance()
 
-                    if (image != -1) {
-                        logo.apply {
-                            setImageResource(image)
-                            isVisible = true
+                        if (image != -1) {
+                            logo.apply {
+                                setImageResource(image)
+                                isVisible = true
+                            }
                         }
+                    } finally {
+                        recycle()
                     }
-                } finally {
-                    recycle()
                 }
-            }
         }
     }
 
@@ -100,50 +105,61 @@ class OnboardingStepView @JvmOverloads constructor(
             view.setOnLongClickListener(null)
         }
 
+        setupAccessibilityOptimization()
+    }
+
+    private fun setupAccessibilityOptimization() {
         // Accessibility optimization:
         // Detect in "footer" and "text" view for text links if there are 2 or less links if so make those links
         // nonclickable and add special "double tap" and "long press" handling as optimization for talkback users.
         if (context.isTalkBackActive) {
-            listOf(footer, text).forEach { view ->
-                // Check it text has links aka URLSpans
-                (view.text as? SpannedString)?.let { span ->
-                    val urls = span.getSpans<URLSpan>()
-                    if (urls.size in 1..2) {
-                        urls.map { url ->
-                            // filter those out with uri
-                            val start = span.getSpanStart(url)
-                            val end = span.getSpanEnd(url)
-                            span.subSequence(start, end) to url.url
-                        }.forEachIndexed { index, (label, url) ->
-                            // Add the special accessibility handling
-                            val action =
-                                view.context.getString(R.string.Snabble_Onboarding_Deeplink_accessibility, label)
-                            if (index == 0) {
-                                view.accessibility.setClickAction(action) {
-                                    showDeeplink(view.context, url)
-                                }
-                            } else {
-                                view.accessibility.setLongClickAction(action) {
-                                    showDeeplink(view.context, url)
-                                }
-                            }
-                        }
-                        // The part to make the links not clickable since we handle it with talkback
-                        view.apply {
-                            this.text = view.text.toString()
-                            movementMethod = null
-                        }
-                    }
-                }
-            }
+            listOf(footer, text).forEach { view -> setupAccessibilityOptimization(view) }
         } else {
-            //handle link click inside the App,
-            //FIXME: Edge case on Android 7 fix with bottomNavigationBar, remains white after backpress
+            // handle link click inside the App,
+            // FIXME: Edge case on Android 7 fix with bottomNavigationBar, remains white after backpress
             listOf(footer, text).forEach { view ->
                 view.movementMethod = LinkClickListener { url ->
                     showDeeplink(view.context, url.toString())
                 }
             }
+        }
+    }
+
+    private fun setupAccessibilityOptimization(view: TextView) {
+        val spannedString = view.text as? SpannedString ?: return
+
+        // Check it text has links aka URLSpans
+        val urls = spannedString.getSpans<URLSpan>()
+        if (urls.size !in 1..2) return
+
+        urls
+            .map { url ->
+                // filter those out with uri
+                val start = spannedString.getSpanStart(url)
+                val end = spannedString.getSpanEnd(url)
+                spannedString.subSequence(start, end) to url.url
+            }
+            .forEachIndexed { index, (label, url) ->
+                // Add the special accessibility handling
+                val action = context.getString(
+                    R.string.Snabble_Onboarding_Deeplink_accessibility,
+                    label
+                )
+                if (index == 0) {
+                    view.accessibility.setClickAction(action) {
+                        showDeeplink(context, url)
+                    }
+                } else {
+                    view.accessibility.setLongClickAction(action) {
+                        showDeeplink(context, url)
+                    }
+                }
+            }
+
+        // The part to make the links not clickable since we handle it with talkback
+        view.apply {
+            this.text = view.text.toString()
+            movementMethod = null
         }
     }
 }
