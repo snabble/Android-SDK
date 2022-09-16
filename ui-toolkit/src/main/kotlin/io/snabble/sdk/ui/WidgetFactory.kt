6fce249afe65cd720b2cc09862e3901187ecc89f
Christian Maier <christian.maier@snabble.io>
2022-09-16 13:18:50 +0200

    Remove broken import

--- ui-toolkit/src/main/kotlin/io/snabble/sdk/ui/WidgetFactory.kt
+++ ui-toolkit/src/main/kotlin/io/snabble/sdk/ui/WidgetFactory.kt
@@ -1,7 +1,6 @@
 package io.snabble.sdk.ui
 
 import io.snabble.sdk.domain.Widget
-import io.snabble.sdk.home.WidgetClick
 
 interface WidgetFactory {
     fun createWidget(widget: Widget, click: WidgetClick? = null) {}
