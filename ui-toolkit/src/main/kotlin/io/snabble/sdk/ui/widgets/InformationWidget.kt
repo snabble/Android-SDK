945c97224b23da38d35def0bc03379de65fcccbb
Fabian Bender <fabian.bender@snabble.io>
2022-09-25 10:38:57 +0200

    change surface to card widget

--- ui-toolkit/src/main/kotlin/io/snabble/sdk/ui/widgets/InformationWidget.kt
+++ ui-toolkit/src/main/kotlin/io/snabble/sdk/ui/widgets/InformationWidget.kt
@@ -1,21 +1,21 @@
 package io.snabble.sdk.ui.widgets
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.ripple.rememberRipple
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -23,6 +23,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import io.snabble.sdk.domain.InformationItem
 import io.snabble.sdk.domain.Padding
+import io.snabble.sdk.ui.WidgetClick
 import io.snabble.sdk.ui.toPaddingValues
 import io.snabble.sdk.ui.toolkit.R
 
@@ -35,26 +36,28 @@ fun InformationWidgetPreview() {
             text = "Füge deine Kundenkarte hinzu.",
             imageSource = R.drawable.store_logo,
             padding = Padding(start = 16, top = 8, end = 16, bottom = 8),
-        )
+        ),
+        onclick = {}
     )
 }
 
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterialApi::class)
 @Composable
 fun InformationWidget(
     modifier: Modifier = Modifier,
-    model: InformationItem
+    model: InformationItem,
+    onclick: WidgetClick
 ) {
-    Surface(
-        shadowElevation = 4.dp,
+    rememberRipple()
+    Card(
+        onClick = { onclick },
+        modifier = modifier,
         shape = RoundedCornerShape(8.dp),
-        color = Color.White,
-        modifier = modifier
-            .clip(RoundedCornerShape(8.dp))
-            .clickable(
-                interactionSource = remember { MutableInteractionSource() },
-                indication = rememberRipple()
-            ) {}
-    ) {
+        backgroundColor = Color.White,
+        elevation = 4.dp,
+        interactionSource = remember {
+            MutableInteractionSource()
+        }) {
         Row(
             modifier = modifier
                 .fillMaxWidth()
