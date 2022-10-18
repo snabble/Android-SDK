33bbe18b9f2ea57a926c5fdd13fdcb8c4c29061d
Fabian Bender <fabian.bender@snabble.io>
2022-10-18 12:52:04 +0200

    refactored toggle viewmodel

--- ui-toolkit/src/main/kotlin/io/snabble/sdk/widgets/snabble/toggle/viewmodel/ToggleViewModel.kt
+++ ui-toolkit/src/main/kotlin/io/snabble/sdk/widgets/snabble/toggle/viewmodel/ToggleViewModel.kt
@@ -4,12 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import io.snabble.sdk.widgets.snabble.toggle.usecases.GetToggleStateUseCase
 import io.snabble.sdk.widgets.snabble.toggle.usecases.SaveToggleStateUseCase
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.stateIn
 
 internal class ToggleViewModel(
@@ -17,19 +12,12 @@ internal class ToggleViewModel(
     private val saveToggle: SaveToggleStateUseCase,
 ) : ViewModel() {
 
-    private val _toggleState = MutableStateFlow(false)
-    val toggleState: StateFlow<Boolean> = _toggleState.asStateFlow()
-
-    init {
-        getToggleState()
-            .stateIn(
-                scope = viewModelScope,
-                initialValue = false,
-                started = SharingStarted.WhileSubscribed(5000),
-            )
-            .onEach(_toggleState::emit)
-            .launchIn(viewModelScope)
-    }
+    val toggleState = getToggleState()
+        .stateIn(
+            scope = viewModelScope,
+            initialValue = false,
+            started = SharingStarted.WhileSubscribed(5000),
+        )
 
     fun saveToggleState(isChecked: Boolean) {
         saveToggle(isChecked)
