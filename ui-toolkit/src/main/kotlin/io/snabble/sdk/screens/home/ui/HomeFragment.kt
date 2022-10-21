acffe3f6daa21ca41f973be652f4bfd72f1e66a6
Fabian Bender <fabian.bender@snabble.io>
2022-10-21 14:26:02 +0200

    rename background color for dynamicScreen

--- ui-toolkit/src/main/kotlin/io/snabble/sdk/screens/home/ui/HomeFragment.kt
+++ ui-toolkit/src/main/kotlin/io/snabble/sdk/screens/home/ui/HomeFragment.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.background
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import io.snabble.sdk.dynamicview.theme.ThemeWrapper
 import io.snabble.sdk.dynamicview.ui.DynamicScreen
 import io.snabble.sdk.screens.home.viewmodel.DynamicHomeViewModel
+import io.snabble.sdk.utils.getComposeColor
 
 class HomeFragment : Fragment() {
 
@@ -21,7 +27,11 @@ class HomeFragment : Fragment() {
             setContent {
                 setViewCompositionStrategy(DisposeOnViewTreeLifecycleDestroyed)
                 ThemeWrapper {
-                    DynamicScreen(dynamicViewModel = viewModel)
+                    DynamicScreen(dynamicViewModel = viewModel,
+                        modifier = Modifier.background(
+                            Color(LocalContext.current.getComposeColor("home_background") ?: Color.Magenta.toArgb())
+                        ))
+
                 }
             }
         }
