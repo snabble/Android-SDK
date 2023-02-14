1d5b98a783c1f0ff841c56a37457e8db2d07cac2
Fabian Bender <fabian.bender@snabble.io>
2023-02-14 11:50:22 +0100

    convert accessibility toolbox to kts

---
+++ accessibility-toolbox/build.gradle.kts
@@ -0,0 +1,69 @@
+@file:Suppress("UnstableApiUsage")
+
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+    id("org.jetbrains.dokka")
+    id("org.jetbrains.kotlin.plugin.parcelize")
+}
+
+apply {
+    from("../scripts/maven.gradle")
+}
+
+description = "Accessibility-Toolbox: Make it easy to make your app accessibile"
+
+android {
+    namespace = "io.snabble.accessibility"
+
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 32
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            postprocessing {
+                isRemoveUnusedCode = false
+                isRemoveUnusedResources = false
+                isObfuscate = false
+                isOptimizeCode = false
+                proguardFile("proguard-rules.pro")
+            }
+        }
+    }
+
+    compileOptions {
+        isCoreLibraryDesugaringEnabled = true
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    sourceSets {
+        getByName("main").java.srcDirs("src/main/kotlin")
+        getByName("test").java.srcDirs("src/test/kotlin")
+        getByName("androidTest").java.srcDirs("src/androidTest/kotlin")
+    }
+
+    kotlinOptions {
+        freeCompilerArgs = listOf(
+            *kotlinOptions.freeCompilerArgs.toTypedArray(),
+            "-Xjvm-default=all"
+        )
+        jvmTarget = "1.8"
+    }
+
+    lint.disable.add("LabelFor")
+    lint.disable.add("MissingTranslation")
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:1.1.5")
+
+    implementation(project(":utils"))
+
+    implementation("androidx.core:core-ktx:1.8.0")
+}
