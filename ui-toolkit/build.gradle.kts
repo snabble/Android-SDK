15188997a3e1263efdb354a45af90fd527384708
Fabian Bender <fabian.bender@snabble.io>
2023-04-21 12:56:51 +0200

    downgrade resaca dep

--- ui-toolkit/build.gradle.kts
+++ ui-toolkit/build.gradle.kts
@@ -46,8 +46,8 @@ android {
 
     compileOptions {
         isCoreLibraryDesugaringEnabled = true
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
 
     kotlinOptions {
@@ -55,7 +55,7 @@ android {
             *kotlinOptions.freeCompilerArgs.toTypedArray(),
             "-Xjvm-default=all"
         )
-        jvmTarget = "17"
+        jvmTarget = "1.8"
     }
 
     buildFeatures {
