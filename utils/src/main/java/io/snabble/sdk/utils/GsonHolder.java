3be2687ceac3756270f6386c9532bc9dea2cf786
Christian Maier <christian.maier@snabble.io>
2023-06-27 15:29:45 +0200

    Remove FileExclusionStrategy due to @Transient

--- utils/src/main/java/io/snabble/sdk/utils/GsonHolder.java
+++ utils/src/main/java/io/snabble/sdk/utils/GsonHolder.java
@@ -3,12 +3,8 @@ package io.snabble.sdk.utils;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-import io.snabble.sdk.utils.gson.FileExclusionStrategy;
-
 public class GsonHolder {
-    private static final Gson INSTANCE = new GsonBuilder()
-            .addSerializationExclusionStrategy(new FileExclusionStrategy())
-            .create();
+    private static final Gson INSTANCE = new GsonBuilder().create();
 
     public static Gson get() {
         return INSTANCE;
