e67192abdd051fefdd394fbe83eb350d198602e6
Fabian Bender <fabian.bender@snabble.io>
2023-07-07 08:53:52 +0200

    upgrade target version

--- kotlin-sample/build.gradle.kts
+++ kotlin-sample/build.gradle.kts
@@ -30,8 +30,8 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
@@ -39,7 +39,7 @@ android {
             *kotlinOptions.freeCompilerArgs.toTypedArray(),
             "-Xjvm-default=all"
         )
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 
 
