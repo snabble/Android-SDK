95603d812684dac53f65d8a6fd98470b6eded182
Christian Maier <christian.maier@snabble.io>
2023-07-17 15:35:52 +0200

    Update Datatrans Android SDK and the repo url

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,7 @@ allprojects {
     repositories {
         google()
         mavenCentral()
-        maven(url = "https://datatrans.jfrog.io/artifactory/mobile-sdk/")
+        maven(url = "https://weareplanet.jfrog.io/artifactory/mobile-sdk/")
         maven(url = "https://jitpack.io")
     }
 
