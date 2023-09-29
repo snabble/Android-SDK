3ba2682605b0d4a654d2ce084400cb994dbad319
Christian Maier <christian.maier@snabble.io>
2023-09-29 14:11:35 +0200

    Remove logging class from ui-toolkit

--- core/src/main/java/io/snabble/sdk/extensions/Logging.kt
+++ core/src/main/java/io/snabble/sdk/extensions/Logging.kt
@@ -4,7 +4,7 @@ import android.util.Log
 
 @Deprecated("Only for debugging", replaceWith = ReplaceWith("this"))
 fun <T> T.xx(msg: String? = null, tag: String = "xx", throwable: Throwable? = null): T {
-    Log.d(tag, "${msg ?: ""} <$this>", throwable)
+    Log.d(tag, "${msg ?: ""}<$this>", throwable)
     return this
 }
 
