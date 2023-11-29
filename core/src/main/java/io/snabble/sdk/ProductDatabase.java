8fb6212a3938703687eed4c469d0b159d5cbd9f0
Christian Maier <christian.maier@snabble.io>
2023-11-29 07:22:25 +0100

    Revert "Fix another file moving crash on lower API levels (#157)"
    This reverts commit 666c1e53da8a2748519cd805accffc05e13381dd.

--- core/src/main/java/io/snabble/sdk/ProductDatabase.java
+++ core/src/main/java/io/snabble/sdk/ProductDatabase.java
@@ -28,7 +28,6 @@ import io.snabble.sdk.codes.ScannedCode;
 import io.snabble.sdk.codes.templates.CodeTemplate;
 import io.snabble.sdk.utils.Dispatch;
 import io.snabble.sdk.utils.Downloader;
-import io.snabble.sdk.utils.FileUtilsSupport;
 import io.snabble.sdk.utils.Logger;
 import io.snabble.sdk.utils.StringNormalizer;
 
@@ -697,7 +696,7 @@ public class ProductDatabase {
             File dbFile = application.getDatabasePath(dbName);
 
             if (!dbFile.exists() || application.deleteDatabase(dbFile.getName())) {
-                FileUtilsSupport.moveFile(otherDbFile, dbFile);
+                FileUtils.moveFile(otherDbFile, dbFile);
                 application.deleteDatabase(otherDbFile.getName());
             }
 
