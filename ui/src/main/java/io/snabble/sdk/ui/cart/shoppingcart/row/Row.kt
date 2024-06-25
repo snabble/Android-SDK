797992555b5edfe9c6864331bc072eb8922dcc51
Fabian Bender <fabian.bender@snabble.io>
2024-06-25 15:30:57 +0200

    add properties to Row required for the ui

--- ui/src/main/java/io/snabble/sdk/ui/cart/shoppingcart/row/Row.kt
+++ ui/src/main/java/io/snabble/sdk/ui/cart/shoppingcart/row/Row.kt
@@ -13,22 +13,30 @@ interface Row {
 data class SimpleRow(
     override val item: ShoppingCart.Item? = null,
     override val isDismissible: Boolean = false,
-    val text: String? = null,
     val title: String? = null,
+    val discount: String? = null,
+    val name: String? =null,
     @DrawableRes val imageResId: Int = 0
 ) : Row
 
 data class ProductRow(
     override val item: ShoppingCart.Item?,
     override val isDismissible: Boolean,
+    val discounts: List<Discount> = mutableListOf(),
     val name: String? = null,
     val subtitle: String? = null,
     val imageUrl: String? = null,
     val encodingUnit: Unit? = null,
     val priceText: String? = null,
+    val depositPrice: String? = null,
+    val depositText: String? = null,
     val quantityText: String? = null,
     val quantity: Int = 0,
     val editable: Boolean = false,
     val manualDiscountApplied: Boolean = false,
+) : Row
 
-    ) : Row
+data class Discount(
+    val name: String,
+    val discount: String,
+)
