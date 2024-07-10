d228fd2052c7917d4abce479f63cd98210fa3799
Fabian Bender <fabian.bender@snabble.io>
2024-07-10 21:50:37 +0200

    Add previews to ShoppingCartScreen

--- ui/src/main/java/io/snabble/sdk/ui/cart/shoppingcart/ShoppingCartScreen.kt
+++ ui/src/main/java/io/snabble/sdk/ui/cart/shoppingcart/ShoppingCartScreen.kt
@@ -11,16 +11,20 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.rememberNestedScrollInteropConnection
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
+import io.snabble.sdk.Product
+import io.snabble.sdk.checkout.LineItem
 import io.snabble.sdk.shoppingcart.ShoppingCart
 import io.snabble.sdk.ui.cart.shoppingcart.cartdiscount.CartDiscount
 import io.snabble.sdk.ui.cart.shoppingcart.cartdiscount.model.CartDiscountItem
 import io.snabble.sdk.ui.cart.shoppingcart.product.DeletableProduct
+import io.snabble.sdk.ui.cart.shoppingcart.product.model.DepositItem
+import io.snabble.sdk.ui.cart.shoppingcart.product.model.DiscountItem
 import io.snabble.sdk.ui.cart.shoppingcart.product.model.ProductItem
 import io.snabble.sdk.ui.utils.ThemeWrapper
 
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun ShoppingCartScreen(
     viewModel: ShoppingCartViewModel = viewModel(),
@@ -28,43 +32,228 @@ fun ShoppingCartScreen(
 ) {
     val uiState = viewModel.uiState.collectAsStateWithLifecycle().value
     ThemeWrapper {
-        LazyColumn(
-            modifier = Modifier.nestedScroll(rememberNestedScrollInteropConnection())
-        ) {
-            items(items = uiState.items, key = { it.hashCode() }) { cartItem ->
-                when (cartItem) {
-                    is ProductItem -> {
-                        DeletableProduct(
-                            modifier = Modifier.animateItemPlacement(),
-                            item = cartItem,
-                            onItemDeleted = {
-                                viewModel.onEvent(
-                                    RemoveItem(
-                                        item = cartItem.item,
-                                        onSuccess = { index ->
-                                            onItemDeleted(cartItem.item, index)
-                                        }
-                                    )
-                                )
-                            },
-                            onQuantityChanged = { quantity ->
-                                viewModel.onEvent(UpdateQuantity(cartItem.item, quantity))
-                            }
-                        )
-                        HorizontalDivider()
-                    }
+        ShoppingCartScreen(
+            uiState = uiState,
+            onItemDeleted = { item ->
+                viewModel.onEvent(
+                    RemoveItem(
+                        item = item,
+                        onSuccess = { index ->
+                            onItemDeleted(item, index)
+                        }
+                    )
+                )
+            },
+            onQuantityChanged = { item, quantity ->
+                viewModel.onEvent(UpdateQuantity(item, quantity))
+            }
+        )
+    }
+}
 
-                    is CartDiscountItem -> {
-                        CartDiscount(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .background(MaterialTheme.colorScheme.background),
-                            item = cartItem
-                        )
-                        HorizontalDivider()
-                    }
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun ShoppingCartScreen(
+    uiState: UiState,
+    onItemDeleted: (ShoppingCart.Item) -> Unit,
+    onQuantityChanged: (ShoppingCart.Item, Int) -> Unit
+) {
+
+    LazyColumn(
+        modifier = Modifier.nestedScroll(rememberNestedScrollInteropConnection())
+    ) {
+        items(items = uiState.items, key = { it.hashCode() }) { cartItem ->
+            when (cartItem) {
+                is ProductItem -> {
+                    DeletableProduct(
+                        modifier = Modifier.animateItemPlacement(),
+                        item = cartItem,
+                        onItemDeleted = { onItemDeleted(cartItem.item) },
+                        onQuantityChanged = { quantity ->
+                            onQuantityChanged(cartItem.item, quantity)
+                        }
+                    )
+                    HorizontalDivider()
+                }
+
+                is CartDiscountItem -> {
+                    CartDiscount(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .background(MaterialTheme.colorScheme.background),
+                        item = cartItem
+                    )
+                    HorizontalDivider()
                 }
             }
         }
     }
+}
+
+@Preview
+@Composable
+fun CardWithDefaultItems() {
+    val uiState = UiState(
+        items = listOf(
+            ProductItem(
+                ShoppingCart.Item(ShoppingCart(), LineItem(name = "Cold brew Coffee")),
+                imageUrl = "https://demodata.snabble.io/demodata/images/snabble-cold-brew-coffee.png",
+                name = "Cold brew Coffee",
+                quantity = 1,
+                editable = true,
+                quantityText = "1",
+                totalPrice = 999,
+                totalPriceText = "9,99€",
+                unit = "g"
+            ),
+            ProductItem(
+                ShoppingCart.Item(ShoppingCart(), LineItem(name = "Energy Drink")),
+                imageUrl = "https://demodata.snabble.io/demodata/images/sdk/energydrink.png",
+                name = "Energy Drink",
+                quantity = 3,
+                editable = true,
+                isAgeRestricted = true,
+                minimumAge = 18,
+                quantityText = "3",
+                totalPrice = 100,
+                totalPriceText = "3,00€",
+                unit = "g"
+            )
+        )
+    )
+
+
+    ShoppingCartScreen(
+        uiState = uiState,
+        onItemDeleted = {},
+        onQuantityChanged = { _, _ -> }
+    )
+}
+
+@Preview
+@Composable
+fun CardWithUserWeightedItems() {
+    val uiState = UiState(
+        items = listOf(
+            ProductItem(
+                ShoppingCart.Item(ShoppingCart(), LineItem(name = "Cold brew Coffee"))
+                    .apply { product = Product.Builder().setType(Product.Type.UserWeighed).build() },
+                name = "Apple",
+                quantity = 1,
+                editable = true,
+                quantityText = "1",
+                totalPrice = 999,
+                totalPriceText = "9,99€",
+                unit = "g"
+            )
+        )
+    )
+
+
+    ShoppingCartScreen(
+        uiState = uiState,
+        onItemDeleted = {},
+        onQuantityChanged = { _, _ -> }
+    )
+}
+
+@Preview
+@Composable
+fun CardWithDiscountItem() {
+    val uiState = UiState(
+        items = listOf(
+            ProductItem(
+                ShoppingCart.Item(ShoppingCart(), LineItem(name = "Cold brew Coffee")),
+                imageUrl = "https://demodata.snabble.io/demodata/images/snabble-cold-brew-coffee.png",
+                name = "Cold brew Coffee",
+                quantity = 1,
+                editable = true,
+                quantityText = "1",
+                discounts = listOf(
+                    DiscountItem(
+                        name = "5€ Discount",
+                        discount = "-5,00€",
+                        discountValue = -500
+                    )
+                ),
+                discountedPrice = "4,99€",
+                totalPrice = 999,
+                totalPriceText = "9,99€",
+                unit = "g"
+            )
+        )
+    )
+
+
+    ShoppingCartScreen(
+        uiState = uiState,
+        onItemDeleted = {},
+        onQuantityChanged = { _, _ -> }
+    )
+}
+
+@Preview
+@Composable
+fun CardWithDeposit() {
+    val uiState = UiState(
+        items = listOf(
+            ProductItem(
+                ShoppingCart.Item(ShoppingCart(), LineItem(name = "Cold brew Coffee")),
+                imageUrl = "https://demodata.snabble.io/demodata/images/snabble-cold-brew-coffee.png",
+                name = "Cold brew Coffee",
+                quantity = 1,
+                editable = true,
+                quantityText = "1",
+                deposit = DepositItem(
+                    depositPrice = 25,
+                    depositPriceText = "0,25€",
+                    depositText = "Deposit"
+                ),
+                discountedPrice = "4,99€",
+                totalPrice = 999,
+                priceText = "4,99€",
+                totalPriceText = "5,24€",
+                unit = "g"
+            )
+        )
+    )
+
+
+    ShoppingCartScreen(
+        uiState = uiState,
+        onItemDeleted = {},
+        onQuantityChanged = { _, _ -> }
+    )
+}
+
+@Preview
+@Composable
+fun CardWithCartDiscount() {
+    val uiState = UiState(
+        items = listOf(
+            ProductItem(
+                ShoppingCart.Item(ShoppingCart(), LineItem(name = "Cold brew Coffee")),
+                imageUrl = "https://demodata.snabble.io/demodata/images/snabble-cold-brew-coffee.png",
+                name = "Cold brew Coffee",
+                quantity = 1,
+                editable = true,
+                quantityText = "1",
+                totalPrice = 999,
+                totalPriceText = "9,99€",
+                unit = "g"
+            ),
+            CartDiscountItem(
+                item = ShoppingCart.Item(ShoppingCart(), LineItem(name = "Multi shopper discount")),
+                name = "Multi shopper discount",
+                discount = "-5,00€"
+            )
+        )
+    )
+
+
+    ShoppingCartScreen(
+        uiState = uiState,
+        onItemDeleted = {},
+        onQuantityChanged = { _, _ -> }
+    )
 }
