04aa80de48fe057807bbe9e1fd397a4139496baa
Fabian Bender <fabian.bender@snabble.io>
2024-07-11 08:39:54 +0200

    add default value if null and renaming

--- ui/src/main/java/io/snabble/sdk/ui/cart/shoppingcart/Extensions.kt
+++ ui/src/main/java/io/snabble/sdk/ui/cart/shoppingcart/Extensions.kt
@@ -5,16 +5,17 @@ import io.snabble.sdk.Unit.convert
 import io.snabble.sdk.Unit.fromString
 import io.snabble.sdk.checkout.PriceModifier
 import java.math.BigDecimal
+import java.math.RoundingMode
 
 fun PriceModifier.convertPriceModifier(
     amount: Int,
     weightedUnit: String?,
     referencedUnit: String?
 ): Int {
-    val factor = convert(BigDecimal(amount), fromString(weightedUnit), fromString(referencedUnit))
-    val mode = Snabble.checkedInProject.value?.roundingMode
+    val convertedValue = convert(BigDecimal(amount), fromString(weightedUnit), fromString(referencedUnit))
+    val mode = Snabble.checkedInProject.value?.roundingMode ?: RoundingMode.HALF_UP
     val digits = Snabble.checkedInProject.value?.currency?.defaultFractionDigits ?: 0
-    return factor
+    return convertedValue
         .multiply(BigDecimal(price))
         .setScale(digits, mode)
         .toInt()
