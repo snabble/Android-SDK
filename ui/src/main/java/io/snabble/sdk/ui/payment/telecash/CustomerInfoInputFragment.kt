299a150fb99da430fd3e66486287dac9e1b3a521
Fabian Bender <fabian.bender@snabble.io>
2024-08-06 10:23:20 +0200

    fix bug due to missing project id

--- ui/src/main/java/io/snabble/sdk/ui/payment/telecash/CustomerInfoInputFragment.kt
+++ ui/src/main/java/io/snabble/sdk/ui/payment/telecash/CustomerInfoInputFragment.kt
@@ -11,6 +11,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import io.snabble.sdk.PaymentMethod
+import io.snabble.sdk.Snabble
 import io.snabble.sdk.ui.payment.CreditCardInputView
 import io.snabble.sdk.ui.payment.PaymentMethodMetaDataHelper
 import io.snabble.sdk.ui.utils.ThemeWrapper
@@ -52,7 +53,7 @@ class CustomerInfoInputFragment : Fragment() {
                     uiState.isNotEmpty() -> AndroidView(
                         factory = { context ->
                             CreditCardInputView(context)
-                                .apply { load(paymentMethod, uiState) }
+                                .apply { load(Snabble.checkedInProject.value?.id,paymentMethod, uiState) }
                         }
                     )
                 }
