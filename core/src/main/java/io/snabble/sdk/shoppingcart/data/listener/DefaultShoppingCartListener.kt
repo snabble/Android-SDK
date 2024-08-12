13f276af87fb2890db188d519199dff0909cbc95
Christian Maier <christian.maier@snabble.io>
2024-08-12 09:04:31 +0200

    Add empty implementation for the ShoppingCartListener

---
+++ core/src/main/java/io/snabble/sdk/shoppingcart/data/listener/DefaultShoppingCartListener.kt
@@ -0,0 +1,41 @@
+package io.snabble.sdk.shoppingcart.data.listener
+
+import io.snabble.sdk.ViolationNotification
+import io.snabble.sdk.shoppingcart.ShoppingCart
+import io.snabble.sdk.shoppingcart.data.Taxation
+
+interface DefaultShoppingCartListener : ShoppingCartListener {
+
+    override fun onItemAdded(cart: ShoppingCart, item: ShoppingCart.Item) {
+    }
+
+    override fun onQuantityChanged(cart: ShoppingCart, item: ShoppingCart.Item) {
+    }
+
+    override fun onCleared(cart: ShoppingCart) {
+    }
+
+    override fun onItemRemoved(cart: ShoppingCart, item: ShoppingCart.Item, pos: Int) {
+    }
+
+    override fun onProductsUpdated(cart: ShoppingCart) {
+    }
+
+    override fun onPricesUpdated(cart: ShoppingCart) {
+    }
+
+    override fun onCheckoutLimitReached(cart: ShoppingCart) {
+    }
+
+    override fun onOnlinePaymentLimitReached(cart: ShoppingCart) {
+    }
+
+    override fun onTaxationChanged(cart: ShoppingCart, taxation: Taxation) {
+    }
+
+    override fun onViolationDetected(violations: List<ViolationNotification>) {
+    }
+
+    override fun onCartDataChanged(cart: ShoppingCart) {
+    }
+}
