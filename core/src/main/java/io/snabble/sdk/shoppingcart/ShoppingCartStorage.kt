b98d2f0c278226c233f6290e1ebf590864eb413f
Fabian Bender <fabian.bender@snabble.io>
2024-09-02 08:25:26 +0200

    use java lang runtime exception

--- core/src/main/java/io/snabble/sdk/shoppingcart/ShoppingCartStorage.kt
+++ core/src/main/java/io/snabble/sdk/shoppingcart/ShoppingCartStorage.kt
@@ -14,6 +14,7 @@ import java.io.File
 import java.io.FileInputStream
 import java.io.FileOutputStream
 import java.io.IOException
+import java.lang.RuntimeException
 import java.nio.charset.Charset
 import kotlin.time.Duration.Companion.seconds
 
