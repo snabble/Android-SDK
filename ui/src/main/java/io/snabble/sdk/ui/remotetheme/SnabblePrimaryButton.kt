e0588c37701c694be8cddc9824e93c8bc726646d
Fabian Bender <fabian.bender@snabble.io>
2024-09-10 11:59:59 +0200

    replace old primary button with new on to keep colors for states like disables, hovered, etc.

--- ui/src/main/java/io/snabble/sdk/ui/remotetheme/SnabblePrimaryButton.kt
+++ ui/src/main/java/io/snabble/sdk/ui/remotetheme/SnabblePrimaryButton.kt
@@ -2,27 +2,100 @@ package io.snabble.sdk.ui.remotetheme
 
 import android.content.Context
 import android.util.AttributeSet
-import com.google.android.material.button.MaterialButton
-import io.snabble.sdk.Snabble
+import android.util.DisplayMetrics
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import io.snabble.sdk.extensions.xx
 import io.snabble.sdk.ui.R
+import io.snabble.sdk.ui.utils.ThemeWrapper
+import kotlinx.coroutines.flow.MutableStateFlow
 
-/**
- * A default Materialbutton which automatically sets the remote theme colors of the
- * current checked in project.
- */
 class SnabblePrimaryButton @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
-    defStyleAttr: Int = R.attr.materialButtonStyle,
-) : MaterialButton(context, attrs, defStyleAttr) {
+    defStyleAttr: Int = 0,
+) : LinearLayout(context, attrs, defStyleAttr) {
+
+    private var isButtonEnabled = MutableStateFlow(isEnabled)
+    private var textRes: MutableStateFlow<String> = MutableStateFlow(getDefaultString(attrs))
+    private var setHeight: MutableStateFlow<Dp?> = MutableStateFlow(null)
 
     init {
-        setProjectAppTheme()
+        init()
     }
 
-    private fun setProjectAppTheme() {
-        val project = Snabble.checkedInProject.value
-        setBackgroundColor(context.getPrimaryColorForProject(project))
-        setTextColor(context.getOnPrimaryColorForProject(project))
+    private fun init() {
+        inflate(context, R.layout.snabble_primary_button, this).apply {
+
+            findViewById<ComposeView>(R.id.button_container).setContent {
+                ThemeWrapper {
+                    val isEnable = isButtonEnabled.collectAsStateWithLifecycle().value
+                    val text = textRes.collectAsStateWithLifecycle().value
+                    val height = setHeight.collectAsStateWithLifecycle().value
+
+                    Button(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(height ?: ButtonDefaults.MinHeight),
+                        onClick = { callOnClick() },
+                        enabled = isEnable
+                    ) {
+                        Text(text = text)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun getDefaultString(attrs: AttributeSet?): String {
+        context.theme.obtainStyledAttributes(attrs, intArrayOf(android.R.attr.text), 0, 0)
+            .apply {
+                try {
+                    return getString(0) ?: ""
+                } finally {
+                    recycle()
+                }
+            }
+    }
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        if (layoutParams.height != ViewGroup.LayoutParams.WRAP_CONTENT) {
+            setHeight.value = pxToDp(context, layoutParams.height).dp.xx()
+        }
+    }
+
+    fun setText(@StringRes value: Int) {
+        textRes.value = context.getString(value)
+    }
+
+    fun setText(value: String) {
+        textRes.value = value
+    }
+
+    override fun setEnabled(enabled: Boolean) {
+        super.setEnabled(enabled)
+        isButtonEnabled.value = enabled.xx("set it")
+    }
+
+    private fun pxToDp(context: Context, px: Int): Float {
+        val metrics: DisplayMetrics = context.resources.displayMetrics
+        return px / (metrics.densityDpi / BASELINE_DENSITY)
+    }
+
+    companion object {
+
+        private const val BASELINE_DENSITY = 160f
     }
 }
