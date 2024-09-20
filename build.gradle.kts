ca7c3eed8db9ec03f08d352517e254d632b7fce4
Fabian Bender <fabian.bender@snabble.io>
2024-09-20 10:21:59 +0200

    fix gradle deprecations

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 import org.jetbrains.dokka.gradle.AbstractDokkaTask
 import org.jetbrains.dokka.gradle.DokkaTask
 import java.net.URI
+import java.util.Locale
 
 @Suppress("DSL_SCOPE_VIOLATION") plugins {
     alias(libs.plugins.benManesVersions)
@@ -107,7 +108,7 @@ tasks.register("printVersion") {
 }
 
 tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
+    delete(rootProject.layout.buildDirectory)
 }
 
 versionCatalogUpdate {
@@ -118,7 +119,7 @@ versionCatalogUpdate {
 }
 
 fun isNonStable(version: String): Boolean {
-    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase(Locale.getDefault()).contains(it) }
     val regex = "^[0-9,.v-]+(-r)?$".toRegex()
     val isStable = stableKeyword || regex.matches(version)
     return isStable.not()
