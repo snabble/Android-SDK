29cf6a2dfc7ed152ca855a09956c2811732bcbb1
Fabian Bender <fabian.bender@snabble.io>
2024-12-04 21:11:19 +0100

    add required properties to the BackendCartItem

--- core/src/main/java/io/snabble/sdk/shoppingcart/data/item/BackendCartItem.kt
+++ core/src/main/java/io/snabble/sdk/shoppingcart/data/item/BackendCartItem.kt
@@ -1,12 +1,15 @@
 package io.snabble.sdk.shoppingcart.data.item
 
 import androidx.annotation.RestrictTo
+import com.google.gson.annotations.SerializedName
+import io.snabble.sdk.checkout.LineItemType
 
 @RestrictTo(RestrictTo.Scope.LIBRARY)
 data class BackendCartItem(
     val id: String? = null,
     val sku: String? = null,
-    val itemId: String? = null,
+    @JvmField @SerializedName("ItemID") val itemId: String? = null,
+    val type: LineItemType = LineItemType.DEPOSIT_RETURN_VOUCHER,
     @JvmField val scannedCode: String? = null,
     @JvmField val amount: Int = 0,
     @JvmField val weightUnit: String? = null,
