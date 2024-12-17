aabd3a9c1a13141e56fac8187859265da437537b
Fabian Bender <fabian.bender@snabble.io>
2024-12-17 20:20:28 +0100

    add additional checks to make the parsing safe

--- core/src/main/java/io/snabble/sdk/codes/templates/depositReturnVoucher/DepositReturnVoucherProvider.kt
+++ core/src/main/java/io/snabble/sdk/codes/templates/depositReturnVoucher/DepositReturnVoucherProvider.kt
@@ -2,6 +2,7 @@ package io.snabble.sdk.codes.templates.depositReturnVoucher
 
 import com.google.gson.JsonElement
 import io.snabble.sdk.codes.templates.CodeTemplate
+import io.snabble.sdk.utils.Logger
 
 data class DepositReturnVoucherProvider(
     val id: String,
@@ -10,15 +11,22 @@ data class DepositReturnVoucherProvider(
 
     companion object {
 
-        fun fromJsonElement(json: JsonElement): DepositReturnVoucherProvider? {
-            val id = json.getValue("id")
+        fun fromJsonElement(json: JsonElement): DepositReturnVoucherProvider? = try {
+            val id: String? = json.getValue("id")
             val templates = json.asJsonObject["templates"].asJsonArray
-                ?.mapNotNull { it.getValue("template")?.let { template -> CodeTemplate(id, template) } }
-
-            return when {
+                ?.mapNotNull {
+                    it.getValue("template")?.let { template -> CodeTemplate(id, template) }
+                }
+            when {
                 id == null || templates == null -> null
                 else -> DepositReturnVoucherProvider(id = id, templates = templates)
             }
+        } catch (e: IllegalStateException) {
+            Logger.e("Couldn't parse deposit return provider: ${e.message}")
+            null
+        } catch (e: UnsupportedOperationException) {
+            Logger.e("Couldn't parse deposit return provider: ${e.message}")
+            null
         }
     }
 }
