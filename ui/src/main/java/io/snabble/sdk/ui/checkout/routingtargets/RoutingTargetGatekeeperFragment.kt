a041bd4b39f622b40530dadb51ccca44daa3cea6
Fabian Bender <fabian.bender@snabble.io>
2025-08-20 08:40:11 +0200

    amke routing target gate keeper fit window

--- ui/src/main/java/io/snabble/sdk/ui/checkout/routingtargets/RoutingTargetGatekeeperFragment.kt
+++ ui/src/main/java/io/snabble/sdk/ui/checkout/routingtargets/RoutingTargetGatekeeperFragment.kt
@@ -1,6 +1,20 @@
 package io.snabble.sdk.ui.checkout.routingtargets
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import io.snabble.sdk.ui.BaseFragment
 import io.snabble.sdk.ui.R
 
-open class RoutingTargetGatekeeperFragment : BaseFragment(R.layout.snabble_fragment_routing_gatekeeper)
+open class RoutingTargetGatekeeperFragment : BaseFragment(R.layout.snabble_fragment_routing_gatekeeper){
+    override fun onCreateActualView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateActualView(inflater, container, savedInstanceState).apply {
+            this?.fitsSystemWindows = true
+        }
+    }
+}
