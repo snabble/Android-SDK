dcf061cc8f8af7e55d8a50636ce1b819588fce67
Fabian Bender <fabian.bender@snabble.io>
2025-08-26 13:52:05 +0200

    WIP: rewrite asset service

--- core/src/main/java/io/snabble/sdk/assetservice/data/dto/AssetDto.kt
+++ core/src/main/java/io/snabble/sdk/assetservice/data/dto/AssetDto.kt
@@ -1,21 +1,9 @@
 package io.snabble.sdk.assetservice.data.dto
 
+import java.io.InputStream
+
 data class AssetDto(
-    val data: ByteArray,
-) {
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as AssetDto
-
-        if (!data.contentEquals(other.data)) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        return data.contentHashCode()
-    }
-}
+    val name: String,
+    val hash: String,
+    val data: InputStream,
+)
